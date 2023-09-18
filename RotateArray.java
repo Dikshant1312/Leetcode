031ff2eeb0f3ada051683c549a54414ee128d048
Dikshant Buwa <108267332+Dikshant1312@users.noreply.github.com>
2023-09-18 18:43:55 +0530

    Create RotateArray.java

---
+++ RotateArray.java
@@ -0,0 +1,39 @@
+//LeetCode-189(Rotate Array)
+import java.util.Arrays;
+class Solution{
+public void rotate(int [] nums, int k){
+//rotate whole array
+//[1,2,3,4,5,6,7] -> [7,6,5,4,3,2,1]
+reverse(nums,0,nums.length-1);
+//rotate elements till k
+//[7,6,5,4,3,2,1] -> [5,6,7,4,3,2,1]
+reverse(nums,0,k-1);
+//rotate nums.length - k
+//[5,6,7,4,3,2,1] -> [5,6,7,1,2,3,4]
+reverse(nums,k,nums.length-1);
+}
+public void reverse(int [] nums, int start, int end){
+while(start<end){
+int temp = nums[start];
+nums[start] = nums[end];
+nums[end] = temp;
+start++;
+end--;
+}
+}
+}
+
+class RotateArray{
+public static void main(String [] args){
+Solution s = new Solution();
+int [] nums = {1,2,3,4,5,6,7};
+int k = 3;
+
+//Array Before Rotation
+System.out.println("Before Rotation : "+Arrays.toString(nums));
+
+//Array After Rotation
+s.rotate(nums,k);
+System.out.println("After Rotation : "+Arrays.toString(nums));
+}
+}
